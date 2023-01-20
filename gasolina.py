ea069dad40c9802dd172046d344121fc0bba4294
Williamcastro31 <williamccastro92@gmail.com>
2023-01-20 01:48:56 +0000

    Adicionado: gasolina.csv	gasolina.png gasolina.py

---
+++ gasolina.py
@@ -0,0 +1,20 @@
+# código de geração do gráfico 
+
+# baixando pacotes
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# ler arquivo CSV
+df_gasolina = pd.read_csv('gasolina.csv')
+
+# geração do gráfico
+
+grafico_gasolina = sns.lineplot(data=df_gasolina, x='dia', y='venda', palette='mako')
+grafico_gasolina.set(title='Hitórico de Preço da Gasolina', xlabel='Dia', ylabel='Venda');
+
+# salvando imagem
+plt.savefig('/content/wda-ebac/gasolina.png')
+
+%%writefile gasolina.py
